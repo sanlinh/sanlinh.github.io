4198e3605082ada64606d9ea4d1a876c6d35f2a7
sanlinh <u6540187@au.edu>
2025-06-11 10:47:52 +0700

    Add Petstore

---
+++ petstore/script.js
@@ -0,0 +1,7 @@
+function adoptPet() { 
+    // Display a thank-you message to the user
+    alert("Thank you for your interest in adopting! Our team will contact you soon."); 
+    
+    // Placeholder for future logic, such as logging the adoption request
+    // Example: logAdoptionRequest(userDetails);
+} 
